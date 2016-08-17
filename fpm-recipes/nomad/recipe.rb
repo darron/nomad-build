1c34fb311eb2691303a8bc181909feefedb09920
darron froese <darron@froese.org>
2016-08-17 17:24:23 -0600

    Update to 0.4.1

--- fpm-recipes/nomad/recipe.rb
+++ fpm-recipes/nomad/recipe.rb
@@ -1,13 +1,13 @@
 class Nomad < FPM::Cookery::Recipe
   name 'nomad'
 
-  version '0.4.0'
+  version '0.4.1'
   revision '1'
   description 'A Distributed, Highly Available, Datacenter-Aware Scheduler'
 
   homepage 'https://www.nomadproject.io/'
   source "https://releases.hashicorp.com/#{name}/#{version}/#{name}_#{version}_linux_amd64.zip"
-  sha256 'c0479dcc03d35c1b0f6e7be256732431384bec16901a124dd97b2a60d465fb91'
+  sha256 '0cdb5dd95c918c6237dddeafe2e9d2049558fea79ed43eacdfcd247d5b093d67'
 
   maintainer 'Darron Froese <darron@froese.org>'
   vendor 'octohost'
